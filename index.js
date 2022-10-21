8442ba477befa3c1ff68f9f4644fe202ed2c3609
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-21 06:47:59 -0400

    commit tree b0a794e7c71618e400287c9c3558d79dccbec7ef

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById('button')
 function addingEventListener() {
+    input.addEventListener('click', ()=>{ 
+            alert('I was clicked!')
+    })
 }
+addingEventListener()
